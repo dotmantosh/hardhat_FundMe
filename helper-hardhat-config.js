554515a7fcfbc5d078769fea957494bf6cb20b1e
dotmantosh <dotuntosh@gmail.com>
2024-01-15 06:52:21 +0100

    helper-hardhat-config setup

---
+++ helper-hardhat-config.js
@@ -0,0 +1,21 @@
+const networkConfig = {
+  4: {
+    name: "rinkeby",
+    ethUsdPriceFeed: ""
+  },
+  11155111: {
+    name: 'sepolia',
+    ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306"
+  }
+}
+
+const developmentChains = ["hardhat",  "localhost"]
+const DECIMALS = 8
+const INITIAL_ANSWER = 20
+
+module.exports = {
+  networkConfig,
+  developmentChains,
+  DECIMALS,
+  INITIAL_ANSWER
+}
